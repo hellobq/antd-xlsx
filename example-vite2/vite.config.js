f1d0e95514646c1d12153bd8799d9e7ee869ee47
qiang.bai <qiang.bai@sinocontrol.cn>
2024-02-06 18:29:33 +0800

    Fix 预览静态文件地址失败

--- example-vite2/vite.config.js
+++ example-vite2/vite.config.js
@@ -3,12 +3,27 @@ import react from '@vitejs/plugin-react'
 import bundleAnalyzer from "rollup-plugin-bundle-analyzer";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    // bundleAnalyzer(),
-    react()
-  ],
-  server: {
-    host: true
+export default defineConfig(({ command, mode }) => {
+  let base = '/', outDir = 'dist'
+  if (
+    command === 'build' &&
+    mode === 'pages'
+  ) {
+    base = '/antx-xlsx/'
+    outDir = 'dist-pages'
+  }
+
+  return {
+    base,
+    plugins: [
+      // bundleAnalyzer(),
+      react()
+    ],
+    build: {
+      outDir
+    },
+    server: {
+      host: true
+    }
   }
 })
